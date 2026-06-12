1871bd9f08d07ed6cd9c81baf4066844509a3ecb
Vadim Steshenko <Vadim@minddnim.com>
2026-06-12 16:40:46 +0200

    Add multiple dependencies test

--- tests/inject_tests.cpp
+++ tests/inject_tests.cpp
@@ -71,3 +71,70 @@ TEST(InjectTests, ThrowsWhenDependencyIsMissing) {
         std::runtime_error
     );
 }
+
+class InjectLogger {
+public:
+    std::string name() const {
+        return "logger";
+    }
+};
+
+class InjectComplexService {
+public:
+    InjectComplexService(
+        std::shared_ptr<InjectRepository> repository,
+        std::shared_ptr<InjectLogger> logger
+    )
+        : repository_(std::move(repository)),
+          logger_(std::move(logger)) {
+    }
+
+    std::string description() const {
+        return repository_->name() + " + " + logger_->name();
+    }
+
+private:
+    std::shared_ptr<InjectRepository> repository_;
+    std::shared_ptr<InjectLogger> logger_;
+};
+
+TEST(InjectTests, CreatesFactoryWithMultipleDependencies) {
+    drogular::ApplicationServices services;
+
+    services.add<InjectRepository>();
+    services.add<InjectLogger>();
+
+    const auto factory =
+        drogular::inject<
+            InjectComplexService,
+            InjectRepository,
+            InjectLogger
+        >(services);
+
+    const auto service = factory();
+
+    ASSERT_NE(service, nullptr);
+    EXPECT_EQ(service->description(), "repository + logger");
+}
+
+TEST(InjectTests, RegistersInjectedFactoryWithMultipleDependencies) {
+    drogular::ApplicationServices services;
+
+    services.add<InjectRepository>();
+    services.add<InjectLogger>();
+
+    services.addFactory<InjectComplexService>(
+        drogular::ServiceLifetime::Singleton,
+        drogular::inject<
+            InjectComplexService,
+            InjectRepository,
+            InjectLogger
+        >(services)
+    );
+
+    const auto service =
+        services.requireService<InjectComplexService>();
+
+    ASSERT_NE(service, nullptr);
+    EXPECT_EQ(service->description(), "repository + logger");
+}
