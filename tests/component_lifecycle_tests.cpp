e7237c67fcdebeb433c80e944beb691f29fd0baa
Vadim Steshenko <Vadim@minddnim.com>
2026-06-17 11:18:21 +0200

    Add Lifecycle for component tags

--- tests/component_lifecycle_tests.cpp
+++ tests/component_lifecycle_tests.cpp
@@ -1,4 +1,6 @@
 #include <drogular/component.hpp>
+#include <drogular/component_registry.hpp>
+#include <drogular/component_renderer.hpp>
 #include <drogular/testing.hpp>
 
 #include <gtest/gtest.h>
@@ -44,4 +46,47 @@ TEST(ComponentLifecycleTests, CallsLifecycleHooksAroundRender) {
     EXPECT_EQ(events[0], "init");
     EXPECT_EQ(events[1], "render");
     EXPECT_EQ(events[2], "destroy");
+}
+
+class LifecycleTagComponent final : public drogular::Component {
+public:
+    static constexpr auto tag = "LifecycleTag";
+
+    static inline std::vector<std::string> events;
+
+    void onInit(drogular::RenderContext&) override {
+        events.push_back("init");
+    }
+
+    std::string render(drogular::RenderContext&) override {
+        events.push_back("render");
+        return "<span>Tag</span>";
+    }
+
+    void onDestroy(drogular::RenderContext&) override {
+        events.push_back("destroy");
+    }
+};
+
+TEST(ComponentLifecycleTests, CallsLifecycleHooksForComponentTags) {
+    LifecycleTagComponent::events.clear();
+
+    drogular::ComponentRegistry registry;
+    registry.registerComponent<LifecycleTagComponent>();
+
+    drogular::RenderContext context;
+
+    const auto html =
+        drogular::component_renderer::render(
+            "<LifecycleTag />",
+            registry,
+            context
+        );
+
+    EXPECT_EQ(html, "<span>Tag</span>");
+
+    ASSERT_EQ(LifecycleTagComponent::events.size(), 3);
+    EXPECT_EQ(LifecycleTagComponent::events[0], "init");
+    EXPECT_EQ(LifecycleTagComponent::events[1], "render");
+    EXPECT_EQ(LifecycleTagComponent::events[2], "destroy");
 }
